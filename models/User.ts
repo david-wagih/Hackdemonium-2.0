8ed1e27b3243e6e4492fe4698108bcdfeb2de312
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-13 10:56:39 +0200

    adding get User function

---
+++ models/User.ts
@@ -0,0 +1,8 @@
+export type getUserModel = {
+  id: number;
+  username: string;
+  email: string;
+  phone: string;
+  friends: [] | any;
+  goals: [] | any;
+};
---
+++ pages/api/getUser.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+import { getUserModel } from "../../models/User";
+
+// http://localhost:3000/api/getUser
+
+const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const id = req.body.id ? req.body.id : null;
+    let userModel: getUserModel;
+    const userInfo = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    const userGoals = await prisma.goal.findMany({
+      where: {
+        user_id: id,
+      },
+    });
+    const userFriends = await prisma.friends.findMany({
+      where: {
+        user_id: id,
+      },
+      select: {
+        friend_id: true,
+      },
+    });
+    userModel = {
+      id: userInfo!.id,
+      username: userInfo!.username,
+      email: userInfo!.email,
+      phone: userInfo!.phone,
+      friends: userFriends,
+      goals: userGoals,
+    };
+    res.status(200).json(userModel);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default getUser;
