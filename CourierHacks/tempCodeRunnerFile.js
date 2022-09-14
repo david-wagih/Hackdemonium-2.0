bf19a442e87ef98286b9bac3466b76606f39aa26
nnayk <79224511+nnayk@users.noreply.github.com>
2022-09-13 22:36:23 -0700

    API email/SMS notification work

---
+++ CourierHacks/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+npm install @trycourier/courier
---
+++ CourierHacks/testEmail.js
@@ -0,0 +1,49 @@
+// Dependencies to install:
+// $ npm install node-fetch --save
+
+const fetch = require('node-fetch');
+const prompt = require('prompt-sync')();
+require('dotenv').config()
+
+async function sendEmail(name,email)
+{
+const options = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + process.env.API_KEY
+  },
+  body: JSON.stringify({
+    "message": {
+      "template":"SN0H965C314WR8M6ZXWDSG9NYD8Y",
+      "to": {
+        "phone_number": "+14083686126"
+        // "email": email
+      },
+      "data":
+      {
+        "name":name
+      }
+    }
+  })
+};
+
+fetch('https://api.courier.com/send', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+}
+
+async function getInput()
+{
+  const name = prompt("What is your name? ")
+  const email = prompt("What is your email? ")
+  const phone = prompt("What is your phone number? ")
+
+  return [name,email,phone]
+}
+
+// getInput()
+
+module.exports = {getInput}
