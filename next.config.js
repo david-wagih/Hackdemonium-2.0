194414a4aa2bbc03e28c7efccbcb2e92531948e0
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-29 02:04:41 +0200

    fix some URL errors

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   output: "standalone",
   env: {
-    NEXT_PUBLIC_HOST: "http://localhost:3000",
+    NEXT_PUBLIC_HOST: "https://goalden.vercel.app",
   },
 };
 
--- utils/axios.ts
+++ utils/axios.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-//http://localhost:3000/
-// this is the base url for our app
-
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_HOST}`,
 });
